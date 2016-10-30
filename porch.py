943103c8921fbdb2c0a20eb2732ff1ecde5354ec
andynilson <andy@andynilson.com>
2016-10-30 13:31:43 -0700

    first working version

--- porch.py
+++ porch.py
@@ -2,13 +2,14 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
-readPIR=11   #GPIO17
-outputPIR=13 #GPIO27
-
-# GPIO17 (header pin 11)
-# GPIO27 (header pin 13)
-# GPIO22 (header pin 15)
-# GPIO23 (header pin 16)
+readPIR = 11  # GPIO17
+ledStrip = 13  # GPIO27
+ledDebug = 15  # GPIO22
+ledStrip2 = 16 #GPIO23
+# GPIO17 (header pin 11)taken
+# GPIO27 (header pin 13)taken
+# GPIO22 (header pin 15)taken
+# GPIO23 (header pin 16)taken
 # GPIO24 (header pin 18)
 # GPIO25 (header pin 22)
 
@@ -16,29 +17,64 @@ outputPIR=13 #GPIO27
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(readPIR, GPIO.IN)  # Read output from PIR motion sensor
-GPIO.setup(outputPIR, GPIO.OUT)  # LED output pin
+GPIO.setup(ledStrip, GPIO.OUT)  # LEDSTRIP output pin
+GPIO.setup(ledDebug, GPIO.OUT)  # LEDDEBUG output pin
+GPIO.setup(ledStrip2, GPIO.OUT) # LEDSTRIP2 output pin
+
+def debugTrickerTreater(pirValue):
+    if (pirValue == 0):
+        print("No Trick-or-treaters", pirValue)
+    else:
+        print("Yes Trick-or-treaters", pirValue)
+
+    GPIO.output(ledDebug, pirValue)
+
+
 
 try:
     while True:
         i = GPIO.input(readPIR)
-        if i == 0:
-            # When output from motion sensor is LOW
-            print("No Trick-or-treaters", i)
-            GPIO.output(outputPIR, 0)  # Turn OFF LED
+        debugTrickerTreater(i)
 
+        if i == 0:  # When output from motion sensor is LOW
+            GPIO.output(ledStrip, 0)  # Turn OFF LED STRIP
+            time.sleep(1)
         elif i == 1:  # When output from motion sensor is HIGH
-            x=0
-            print("Trick-or-treaters detected", i)
+            x = 0
             # Turn ON LED
-            while x<5:
-                GPIO.output(outputPIR, 1)
-                time.sleep(3)
-                GPIO.output(outputPIR, 0)
-                time.sleep(.1)
-                x=x+1
+            while x < 5:
 
-        time.sleep(1)
-        print (datetime.datetime.now())
+                print("led running loop",x)
+
+                #T=0
+                debugTrickerTreater(GPIO.input(readPIR))
+                GPIO.output(ledStrip, 1)
+                time.sleep(1.3)
+
+                #T=1.3
+                debugTrickerTreater(GPIO.input(readPIR))
+                GPIO.output(ledStrip2, 0)
+                time.sleep(.4)
+
+                #T=1.7
+                debugTrickerTreater(GPIO.input(readPIR))
+                GPIO.output(ledStrip2, 1)
+                time.sleep(1.3)
+
+                #T=3
+                debugTrickerTreater(GPIO.input(readPIR))
+                GPIO.output(ledStrip, 0)
+                time.sleep(.4)
+
+                #T=3.4
+                x += 1
+
+        print("exited led running loop")
+
+        GPIO.output(ledStrip, 0) #not needed
+        GPIO.output(ledStrip2, 0)
+
+        print(datetime.datetime.now())
         print
 finally:
-    GPIO.cleanup()         # clean up after yourself
+    GPIO.cleanup()  # clean up after yourself
